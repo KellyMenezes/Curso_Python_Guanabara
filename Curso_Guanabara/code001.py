b607d6c8637acbbc57dbc5f7ca9ca51094742664
Kelly Menezes <106978926+StarScorpion@users.noreply.github.com>
2023-03-09 10:27:01 -0300

    Update code001.py
    Mudei um código.

--- Curso_Guanabara/code001.py
+++ Curso_Guanabara/code001.py
@@ -1,3 +1,3 @@
 #Crie um programa que leio o nome de uma pessoa e mostre uma mensagem de boas vindas
-Name_People = input("Digite seu nome ,por favor: ")
-print("Hello beautiful {}!".format(Name_People),"Nice to meat you!!!")
+Name_People = input("Digite seu nome, por favor: ")
+print(f"Olá {Name_People}, você é bem vindo(a) aqui.","É um prazer conhecer você!!!")

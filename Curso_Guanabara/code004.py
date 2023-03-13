f401d1c22aa25ebed1642e8ff2588b5cb1aea677
Kelly Menezes <kellymenezes845@gmail.com>
2023-03-13 18:49:13 -0300

    Update 004
    Pequeno erro

--- Curso_Guanabara/code004.py
+++ Curso_Guanabara/code004.py
@@ -1,5 +1,5 @@
 #Faça um programa que leia seu tipo primitivo e mostre tudo sobre ele.
-Pandora = (input("Digite algo: ")
+Pandora = input("Digite algo: ")
 print(type(Pandora))
 print("pandora é ",Pandora.isalpha)
 print(Pandora.isspace)

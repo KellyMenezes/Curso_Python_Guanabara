6b7ca649f4ad9b05d055396a56fc1f5df006a20a
Kelly Menezes <kellymenezes845@gmail.com>
2023-03-21 16:05:46 -0300

    Aula9

--- aula9.py
+++ aula9.py
@@ -1,2 +1,11 @@
 frase = "Curso em video Python"
-print(frase[2:10:2])
+print(len(frase))#escrevera a quantidade de caracters que frase tem
+print(frase[2:10:2])#começara no terceiro caracter terminara no décimo,e pulara a cada 2 espaços
+print(frase[0:12])#começara no zero e terminara no 11
+print(frase.count('a'))#conta quantas vezes um carater aparece
+print(frase.find('vid'))#Fala apartir de qual caracter ele está se tiver,caso ñ encontrar -1
+print(frase.replace("video", "ViDeO"))# troca os caracters pelo que você quer
+print(frase.join("___-"))#coloca um algo entre os caracters além de replicas a variável
+print(frase.capitalize())#transforma todos
+print(frase.upper())
+print(frase.lower())
